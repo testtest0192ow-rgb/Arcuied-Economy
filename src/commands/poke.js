5e6158d47a2605c6dbae9a71afe4c4d4ebbb70d0
testtest0192ow <testtest0192ow@example.com>
2026-08-25 16:49:15 +0300

    remove blackjack/poke/tickle again

--- src/commands/poke.js
+++
@@ -1,8 +0,0 @@
-const { createSocialActionCommand } = require('../utils/socialAction');
-module.exports = createSocialActionCommand({
-  name: 'poke',
-  description: 'Ткнуть пользователя',
-  category: 'poke',
-  message: (a, b) => `${a} тыкает ${b}. 👉`,
-  selfMessage: '{user} тычет сам себя.',
-});
--- src/commands/tickle.js
+++
@@ -1,7 +0,0 @@
-const { createSocialActionCommand } = require('../utils/socialAction');
-module.exports = createSocialActionCommand({
-  name: 'tickle',
-  description: 'Пощекотать пользователя',
-  category: 'tickle',
-  message: (a, b) => `${a} щекочет ${b}! 😆`,
-});
